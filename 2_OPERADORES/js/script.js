71359b03f2c78246e91a4f9f374f1ac4b1389eef
Johne Lima <johne.keyvid0@gmail.com>
2023-11-20 23:03:13 -0300

    Initial commit

---
+++ 2_OPERADORES/js/script.js
@@ -0,0 +1,74 @@
+// 1 - Number
+
+console.log(typeof 2)
+console.log(typeof -10)
+console.log(typeof 1.5)
+
+// 2 - Ops. aritméricas
+
+console.log(2 + 4)
+console.log(10 - 5)
+console.log(5 * 4)
+console.log(10 / 2)
+
+console.log(5 * (8 - 3))
+
+// 3 - Special numbers
+
+console.log(typeof Infinity)
+
+console.log(typeof -Infinity)
+
+console.log( 12 * "asd")
+
+console.olg(typeof NaN)
+
+// 4 - String
+
+console.log("Um texto")
+console.log(`Mais um texto`)
+console.log('13')
+
+console.log(typeof "Um texto")
+console.log(typeof `Mais um texto`)
+
+// 5 - Símbolos especiais em strings
+
+console.log("Testando a \n quebra de linha")
+
+console.log("espaçamento de \t tab")
+
+// 6 - Concatenação
+
+console.log("Oi, " + "tudo " + "bem?")
+
+console.log(`Testando ` + `com ` + `crase!`)
+
+// 7 - Interpolação (Template Strings)
+
+console.log(`A soma de 2 + 2 é : ${2 + 2}`)
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`)
+
+// 8 - Boolean
+
+console.log(true)
+console.log(5 > 20)
+console.log(30 > 10)
+console.log(typeof false)
+
+// 9 - Comparações
+
+console.log( 5 <= 5)
+console.log(5 < 5)
+console.log(10 == 10)
+console.log(10 == 9)
+
+console.log( 10 != 9)
+
+// 10 - Idêntico 
+
+console.log(9 == "9")
+console.log(9 === "9")
+console.log(9 != "9")
+console.log(9 !== "9")
